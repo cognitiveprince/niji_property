1b90f5c7f596327cf11a701a6efc4610d2f203d5
Swikar Adhikari <np03a190013@heraldcollege.edu.np>
2022-05-03 15:15:04 +0545

    perf : 📈 Added Redux Store Persist To Improve Performance

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -2,12 +2,24 @@ import { applyMiddleware, createStore } from "redux";
 import reducers from "./Reducers/index";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 const middleware = [thunk];
 
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 const store = createStore(
-  reducers,
+  persistedReducer,
   composeWithDevTools(applyMiddleware(...middleware))
 );
 
+const persistor = persistStore(store);
+
 export default store;
+export { persistor };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import store from "./Redux/store";
+import store, { persistor } from "./Redux/store";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
