94df3977745cd1353c728fb9217d808e642799e5
Parbat Lama <lamaparbat70@gmail.com>
2022-06-09 13:35:07 +0545

    removed extra line- spaces

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -5,22 +5,14 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB-yV615QBFCZdqAB4JgX3k7gZVaqhqEdc",
-
   authDomain: "niji-2081c.firebaseapp.com",
-
   projectId: "niji-2081c",
-
   storageBucket: "niji-2081c.appspot.com",
-
   messagingSenderId: "520251844092",
-
   appId: "1:520251844092:web:f97a57d0e36b282f7dbe79",
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
-
 export const db = getFirestore(app);
-
 export const storage = getStorage(app);
